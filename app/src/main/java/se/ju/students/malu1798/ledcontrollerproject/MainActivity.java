2ffddc65b8cfb2f1a59cb07ad078f194a5030010
malu1798 <malu1798@student.ju.se>
2019-02-01 18:28:15 +0100

    Added language support for Swedish and Finnish

--- app/src/main/java/se/ju/students/malu1798/ledcontrollerproject/MainActivity.java
+++ app/src/main/java/se/ju/students/malu1798/ledcontrollerproject/MainActivity.java
@@ -1,6 +1,22 @@
 package se.ju.students.malu1798.ledcontrollerproject;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void changeViewButtonClicked(View view){
+        System.out.println("Button clicked");
+        Intent intent = new Intent(this, PickColorActivity.class);
+        startActivity(intent);
+    }
+
 }
