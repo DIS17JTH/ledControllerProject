0ba160221e7ae056c4d3497e0fce40b726198254
malu1798 <malu1798@student.ju.se>
2019-02-05 17:59:22 +0100

    Some layout configuration

--- app/src/main/java/se/ju/students/malu1798/ledcontrollerproject/ModeActivity.java
+++ app/src/main/java/se/ju/students/malu1798/ledcontrollerproject/ModeActivity.java
@@ -3,11 +3,33 @@ package se.ju.students.malu1798.ledcontrollerproject;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class ModeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mode);
+    }
+
+    @Override
+    //if settings menu should show
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    //settings menu
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                /*handle*/
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
--- app/src/main/java/se/ju/students/malu1798/ledcontrollerproject/PickColorActivity.java
+++ app/src/main/java/se/ju/students/malu1798/ledcontrollerproject/PickColorActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class PickColorActivity extends AppCompatActivity {
@@ -17,5 +19,25 @@ public class PickColorActivity extends AppCompatActivity {
         System.out.println("Button clicked");
         Intent intent = new Intent(this, ModeActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    //if settings menu should show
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    //settings menu
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                /*handle*/
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
